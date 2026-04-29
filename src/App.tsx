6b944949521a5d7f031944a850a7e5fb9124463c
Akshay R (A-K-SHA-Y) <akkitricks@gmail.com>
2026-04-29 23:51:35 +0530

    up

--- src/App.tsx
+++ src/App.tsx
@@ -4,29 +4,11 @@ import ScrollTriggered from "./Scroll";
 import { ImageCircles } from "./ImageCircle";
 import Rotate from "./Test";
 
-import OrbitMotion from "./OrbitMotion";
-
-const images = [
-  "https://picsum.photos/300?1",
-  "https://picsum.photos/300?2",
-  "https://picsum.photos/300?3",
-  "https://picsum.photos/300?4",
-  "https://picsum.photos/300?5",
-  "https://picsum.photos/300?6",
-];
 function App() {
   return (
     <main>
       <section>
         <Rotate />
-        <OrbitMotion
-          items={images}
-          radius={220}
-          speed={1}
-          autoRotate={true}
-          itemSize={100}
-          depth={120}
-        />
       </section>
       <section>
         <ImageCircles />

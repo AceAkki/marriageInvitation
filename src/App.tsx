cefaae72ffdc91ce2ca11c1f931fd5b3e91bdf7d
Akshay R (A-K-SHA-Y) <akkitricks@gmail.com>
2026-05-01 01:41:35 +0530

    content update

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,34 @@ import ScrollTriggered from "./Scroll";
 import { ImageCircles } from "./ImageCircle";
 import Rotate from "./Test";
 
+import GaneshaImg from "./assets/ganesha.png";
+
 function App() {
   return (
     <main>
-      <section style={{ background: "#DCAE96" }}>
-        <h1 className="name-title gold-text"> Akshay </h1>
+      <section></section>
+      <section>
+        <div className="card-layout">
+          <div className="ganesha-wrap">
+            <p className="gold-text">ॐ श्री गणेशाय नम</p>
+            <img src={GaneshaImg} className="ganesha-img" />
+          </div>
+          <div className="parents-wrap">
+            <h5 className="parent-name">Smt. Sonali & Shri Ramdas Patil </h5>
+            <p>along with</p>
+            <h5 className="parent-name">Smt. Rekha Sudhakar Bhadrige</h5>
+            <p>
+              together with their families ask you to join them for the wedding
+              of
+            </p>
+          </div>
+
+          <div className="couple-wrap">
+            <h1 className="name-title gold-text"> Akshay </h1>
+            <h3 className="name-sub-title gold-text">And</h3>
+            <h1 className="name-title gold-text"> Divyani </h1>
+          </div>
+        </div>
       </section>
       <section>
         <Rotate />

455de464a939c3ddd86a9959c232be92aa119f5b
Akshay R (A-K-SHA-Y) <akkitricks@gmail.com>
2026-05-02 21:38:11 +0530

    package update

--- src/App.tsx
+++ src/App.tsx
@@ -9,13 +9,12 @@ import type { LenisRef } from "lenis/react";
 import { cancelFrame, frame } from "framer-motion";
 import { useEffect, useRef } from "react";
 
-import GaneshaImg from "./assets/ganesha.png";
-import ArchBG from "./assets/rose-arc.png";
 import Frame from "./assets/frame.png";
-import FrameSquare from "./assets/frameSquare.png";
 import WeddingInvite from "./FloralGrowth";
 
 import ProCountdown from "./Countdown";
+
+import InviteContent from "./components/InviteContent";
 
 function App() {
   const lenisRef = useRef<LenisRef>(null);
@@ -39,45 +38,7 @@ function App() {
           <WeddingInvite />
         </section>
         <section>
-          <div className="card-layout">
-            <div className="ganesha-wrap">
-              <p className="gold-text">ॐ श्री गणेशाय नम</p>
-              <img src={GaneshaImg} className="ganesha-img" />
-            </div>
-            <div className="parents-wrap">
-              <h5 className="parent-name">Smt. Sonali & Shri Ramdas Patil </h5>
-              <p>along with</p>
-              <h5 className="parent-name">Smt. Rekha Sudhakar Bhadrige</h5>
-              <p>
-                together with their families ask you to join them for the
-                wedding of
-              </p>
-            </div>
-
-            <div className="couple-wrap">
-              <h1 className="name-title gold-text"> Akshay </h1>
-              <h3 className="name-sub-title gold-text">And</h3>
-              <h1 className="name-title gold-text"> Divyani </h1>
-              <img src={ArchBG} className="frame" alt="" />
-            </div>
-
-            <hr className="gold-divide" />
-
-            <div className="date-wrap">
-              <div className="frame-content">
-                <p>Saturday, 27th June, 2026</p>
-                <p>12:30PM : 04:30PM</p>
-
-                <br />
-                <strong>Central Railway Officers Hall</strong>
-                <p>
-                  D/14, Dada Saheb Phalke Marg, Gautam Nagar, Dadar, Mumbai,
-                  Maharashtra 400014
-                </p>
-              </div>
-              <img src={FrameSquare} className="frame" alt="" />
-            </div>
-          </div>
+          <InviteContent />
         </section>
         <section>
           <ProCountdown
---
+++ src/components/InviteContent.tsx
@@ -0,0 +1,48 @@
+import GaneshaImg from "../assets/ganesha.png";
+import ArchBG from "../assets/rose-arc.png";
+import FrameSquare from "../assets/frameSquare.png";
+
+const InviteContent = () => {
+  return (
+    <div className="card-layout">
+      <div className="ganesha-wrap">
+        <p className="gold-text">ॐ श्री गणेशाय नम</p>
+        <img src={GaneshaImg} className="ganesha-img" />
+      </div>
+      <div className="parents-wrap">
+        <h5 className="parent-name">Smt. Sonali & Shri Ramdas Patil </h5>
+        <p>along with</p>
+        <h5 className="parent-name">Smt. Rekha Sudhakar Bhadrige</h5>
+        <p>
+          together with their families ask you to join them for the wedding of
+        </p>
+      </div>
+
+      <div className="couple-wrap">
+        <h1 className="name-title gold-text"> Akshay </h1>
+        <h3 className="name-sub-title gold-text">And</h3>
+        <h1 className="name-title gold-text"> Divyani </h1>
+        <img src={ArchBG} className="frame" alt="" />
+      </div>
+
+      <hr className="gold-divide" />
+
+      <div className="date-wrap">
+        <div className="frame-content">
+          <p>Saturday, 27th June, 2026</p>
+          <p>12:30PM : 04:30PM</p>
+
+          <br />
+          <strong>Central Railway Officers Hall</strong>
+          <p>
+            D/14, Dada Saheb Phalke Marg, Gautam Nagar, Dadar, Mumbai,
+            Maharashtra 400014
+          </p>
+        </div>
+        <img src={FrameSquare} className="frame" alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default InviteContent;
